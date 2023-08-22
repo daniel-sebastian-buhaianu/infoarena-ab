b7c7967df28e42909e517e7cdec2e3eb6e9f0632
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-08-22 12:38:03 +0300

    Update ab.c

--- ab.c
+++ ab.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main()
+{
+	int a, b;
+	FILE *fin, *fout;
+
+	fin = fopen("adunare.in", "r");
+
+	if (!fin) return 1;
+
+	fscanf(fin, "%d", &a);
+	fscanf(fin, "%d", &b);
+	
+	fclose(fin);
+
+	fout = fopen("adunare.out", "w");
+	
+	fprintf(fout, "%d", a + b);
+
+	fclose(fout);
+	
+	return 0;
+}
